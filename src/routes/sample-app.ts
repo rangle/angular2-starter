32ce173fe27052791ea798233e0cb190092f2228
Andrew Lo <andrew.lo@rangle.io>
2016-07-17 01:08:12 -0400

    Set base href to "\" as specified by Angular docs. (#79)
    Give counter route "counter" path and set up a redirect to it as
    the default route.

--- src/routes/sample-app.ts
+++ src/routes/sample-app.ts
@@ -1,8 +1,14 @@
+import { RouterConfig } from '@angular/router';
+
 import { RioCounterPageComponent } from '../containers/counter-page';
 import { RioAboutPageComponent } from '../containers/about-page';
 
-export const SAMPLE_APP_ROUTES = [{
+export const SAMPLE_APP_ROUTES: RouterConfig = [{
   path: '',
+  redirectTo: 'counter',
+  pathMatch: 'full'
+}, {
+  path: 'counter',
   component: RioCounterPageComponent
 }, {
   path: 'about',
