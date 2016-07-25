86b1d30fd058f14cde1f55fadff38e83a20ace19
Purvi Kanal <purvikanal92@gmail.com>
2016-07-25 14:41:33 -0400

    change `sample-app` to `app`
    - Removed counter/ about page
    - Removed dependency on container component
    - Created root-page to satisfy minimal routing

---
+++ src/containers/app.ts
@@ -0,0 +1,20 @@
+import {
+  Component,
+  ViewEncapsulation,
+  Inject,
+  ApplicationRef
+} from '@angular/core';
+
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+@Component({
+  selector: 'rio-app',
+  directives: [
+    ROUTER_DIRECTIVES,
+  ],
+  // Global styles imported in the app component.
+  encapsulation: ViewEncapsulation.None,
+  styles: [ require('../styles/index.css') ],
+  template: require('./app.html'),
+})
+export class RioAppComponent {};
---
+++ src/containers/root-page.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rio-root-page',
+  template: `<h1>Angular 2 + TypeScript setup</h1>`
+})
+export class RioRootPageComponent {}
