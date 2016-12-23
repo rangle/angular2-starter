b45ec753f15f9b1f49d3ba71a66d6da22852dc4b
Andrew Smith <smithad15@users.noreply.github.com>
2016-12-23 17:01:01 -0500

    Add basic angular test bed configuration (#222)

--- src/tests.entry.ts
+++ src/tests.entry.ts
@@ -13,6 +13,17 @@ import 'zone.js/dist/jasmine-patch';
 
 import 'ts-helpers';
 
+import { TestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
+
 const testContext = (require as {context?: Function})
   .context('./', true, /^(.(?!tests\.entry))*\.ts$/);
 
